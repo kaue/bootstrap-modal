4c7439980a6904a1f15896c5b85073f6cd7450a6
Ilya Radchenko <ilya@burstcreations.com>
2015-12-23 11:59:14 -0500

    First commit for working bootstrap-modal

---
+++ app/transitions.js
@@ -0,0 +1,30 @@
+import { target } from 'liquid-tether';
+
+const options = {
+  duration: 700,
+  easing: 'easeInOutQuint'
+};
+
+export default function () {
+  this.transition(
+    target('bootstrap-modal'),
+    // hack to get reverse working..
+    this.toValue(current => {
+      return current.isVisible;
+    }),
+    this.use('explode', {
+      pick: '.modal-dialog',
+      use: ['to-down', options]
+    }, {
+      pick: '.modal-backdrop',
+      use: 'crossFade'
+    }),
+    this.reverse('explode', {
+      pick: '.modal-dialog',
+      use: ['to-up', options]
+    }, {
+      pick: '.modal-backdrop',
+      use: 'crossFade'
+    })
+  );
+}
--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'bootstrap-modal'
+  name: 'bootstrap-modal',
+
+  included: function(app) {
+    this._super.included(app);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    app.import('vendor/bootstrap-modal/styles.css');
+  }
 };
